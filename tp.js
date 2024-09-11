d876c934cda41bbbacc6214d0475af18bcbabe1d
miriam-daianne <miriamdaianne@protonmail.com>
2024-09-11 20:22:20 -0300

    version 11/09

--- tp.js
+++ tp.js
@@ -1,110 +1,48 @@
-// function pontuacao (){
-//     const score = document.querySelectorAll('tbody span.score');
-//         for(let i = 0; i <score.length; i++){
-//             if(score[i].textContent >= 8){
-//                 score[i].style.backgroundColor = '#D0ABCD';
-//             }else if(score[i].textContent >= 6 && score[i].textContent < 8 ){
-//                 score[i].style.backgroundColor = '#F3AAB5';
-//             }else if(score[i].textContent >= 4 && score[i].textContent < 6 ){
-//                 score[i].style.backgroundColor = '#9BAAC9';
-//             }else if(score[i].textContent >= 2 && score[i].textContent < 4 ){
-//                 score[i].style.backgroundColor = '#C6D8E6';
-//             }else if(score[i].textContent >= 0 && score[i].textContent < 2 ){
-//                 score[i].style.backgroundColor = '#FDDAE1' }
-//         }
-//      }
+function scoreColor(){
 
-//      pontuacao();
+    let lines = document.querySelectorAll('tbody tr');
 
+    for(let i = 0; i < lines.length; i++){
 
-// function teste(){
-//     const linhas = document.querySelectorAll('tbody tr');
+        let score = lines[i].querySelectorAll('span.score');
 
-//     linhas.forEach(linha => {
-//         const scores = linha.querySelectorAll('span.score');  // Seleciona os scores dentro da linha atual
+        for( let j = 0; j < score.length; j++){
 
-//         scores.forEach(score => {
-//             const scoreValue = parseFloat(score.textContent);
-
-//             if (scoreValue >= 8) {
-//                 linha.style.backgroundColor = '#D0ABCD';
-//             } else if (scoreValue >= 6 && scoreValue < 8) {
-//                 linha.style.backgroundColor = '#F3AAB5';
-//             } else if (scoreValue >= 4 && scoreValue < 6) {
-//                 linha.style.backgroundColor = '#9BAAC9';
-//             } else if (scoreValue >= 2 && scoreValue < 4) {
-//                 linha.style.backgroundColor = '#C6D8E6';
-//             } else if (scoreValue >= 0 && scoreValue < 2) {
-//                 linha.style.backgroundColor = '#FDDAE1';
-//             }
-//         });
-//     });
-// }
-
-// teste();
-
-
-function teste(){
-    const linhas = document.querySelectorAll('tbody tr');
-
-    for(let i =0; i < linhas.length; i++){
-        let linha = linhas[i];
-
-        const scores = linha.document.querySelectorAll('span.score');
-
-        for(let j = 0; j < scores.length; i++){
-        
-        let score = scores [j];
-
-            if(score.textContent >= 8){
-                                linha.style.backgroundColor = '#D0ABCD';
-                            }else if(score.textContent >= 6 && score[i].textContent < 8 ){
-                                linha.style.backgroundColor = '#F3AAB5';
-                            }else if(score.textContent >= 4 && score[i].textContent < 6 ){
-                                linha.style.backgroundColor = '#9BAAC9';
-                            }else if(score.textContent >= 2 && score[i].textContent < 4 ){
-                                linha.style.backgroundColor = '#C6D8E6';
-                            }else if(score.textContent >= 0 && score[i].textContent < 2 ){
-                                linha.style.backgroundColor = '#FDDAE1' }
-                        }
-
+            if (score[j].textContent >9 && score[j].textContent <=10){
+                lines[i].style.backgroundColor = '#B9D2D7';
+            }else if(score[j].textContent == 9){
+                lines[i].style.backgroundColor = '#CEE1E7';
+            }else if(score[j].textContent >=8 && score[j].textContent <9){
+                lines[i].style.backgroundColor = '#DDE8EC';
+            }else if(score[j].textContent >6 && score[j].textContent <=8){
+                lines[i].style.backgroundColor = '#EAEFF5';
+            }else if(score[j].textContent >=0 && score[j].textContent <=6){
+                lines[i].style.backgroundColor = '#FDEDE0';
+            }
         }
-
     }
+}
 
-    teste();
 
-    // /function teste(){
-    //     const linhas = document.querySelectorAll('tbody tr');
-    
-    //     for(let i =0; i < linhas.length; i++){
-    //         let linha = linhas[i];
-    
-    //         const scores = linha.document.querySelectorAll('span.score');
-    
-    //         for(let j = 0; j < scores.length; i++){
-            
-    //         let score = scores [j];
-    
-    //             if(score.textContent >= 8){
-    //                                 linha.style.backgroundColor = '#D0ABCD';
-    //                             }else if(score.textContent >= 6 && score[i].textContent < 8 ){
-    //                                 linha.style.backgroundColor = '#F3AAB5';
-    //                             }else if(score.textContent >= 4 && score[i].textContent < 6 ){
-    //                                 linha.style.backgroundColor = '#9BAAC9';
-    //                             }else if(score.textContent >= 2 && score[i].textContent < 4 ){
-    //                                 linha.style.backgroundColor = '#C6D8E6';
-    //                             }else if(score.textContent >= 0 && score[i].textContent < 2 ){
-    //                                 linha.style.backgroundColor = '#FDDAE1' }
-    //                         }
-    
-    //         }
-    
-    //     }
-    
-    //     teste();
-    
+scoreColor();
 
+function buttons(){
+    // let legend = document.querySelector('.mdl-style-list-footer box-footer b-t list-statistics');
+    let buttonLegend = document.createElement('button');
+
+    buttonLegend.innerHTML = "Legend";
+
+    buttonLegend.style.backgroundColor = "white";
+    buttonLegend.style.borderRadius = "10px";
+    buttonLegend.style.border = " solid blue 0.5px";
+    buttonLegend.style.padding = "5px";
+
+    // legend.style.display = "flex";
+
+    document.appendChild(buttonLegend);
+}
+
+buttons();
 
 
 
